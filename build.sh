cfec07b0c41dccb65704a53fc568a835a3e59eff
afrmtbl <afrmtbl@gmail.com>
2019-03-15 01:06:07 -0400

    Replaced UglifyJS and UglifyCSS with terset and cssnano (csso)

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cat src/annotationlib/AnnotationParser.js src/annotationlib/AnnotationRenderer.js src/index.js > dist/dist.js
 cat src/annotationlib/AnnotationRenderer.css > dist/dist.css
 
-uglifyjs dist/dist.js > dist/dist.min.js
-uglifycss dist/dist.css > dist/dist.min.css
+terser dist/dist.js > dist/dist.min.js
+csso dist/dist.css > dist/dist.min.css
